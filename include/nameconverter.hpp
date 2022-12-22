a2fdb513c4e667870390cdf18e8a660cbe13b6ae
Timo Früh <timo.frueh@icloud.com>
2022-12-22 14:51:07 +0100

    add rnc::createRolandName

--- include/nameconverter.hpp
+++ include/nameconverter.hpp
@@ -10,4 +10,7 @@ namespace rnc {
 
     // returns a vector containing all the lines of a titles.csv file
     std::vector<std::string> parseTitlesCSV(const std::string& titlesCSVPath);
+
+    // returns a name based on a number which the FP-30 can understand
+    std::string createRolandName(const int& index);
 }
--- src/nameconverter.cpp
+++ src/nameconverter.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
 #include "nameconverter.hpp"
 
 std::vector<std::string> rnc::parseTitlesCSV(const std::string& titlesCSVPath) {
@@ -23,4 +24,36 @@ std::vector<std::string> rnc::parseTitlesCSV(const std::string& titlesCSVPath) {
     ifile.close();
 
     return titlesVector;
+}
+
+std::string rnc::createRolandName(const int& index) {
+    
+    int insertionPos;
+    std::string name;
+    std::string sIndex;
+    std::string insertionCharacter;
+    
+    std::string nameTemplates[3] = { "R068_%.wav", "R068_0%.wav" , "R068_00%.wav" };
+
+    if (index > 999 || index < 1) {
+        throw std::invalid_argument("Roland name index must be between 1 and 999");
+    } 
+    else if (index > 99) {
+        name = nameTemplates[0];
+    }
+    else if (index > 9) {
+        name = nameTemplates[1];
+    }
+    else if (index > 0) {
+        name = nameTemplates[2];
+    }
+
+    insertionCharacter = "%";
+    sIndex = std::to_string(index);
+
+    insertionPos = name.find(insertionCharacter);
+
+    name.replace(insertionPos, 1, sIndex);
+
+    return name;
 }
