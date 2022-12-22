abbcb297b4f89d6b0eae40ac7002d79ef3b15c97
Timo Früh <timo.frueh@icloud.com>
2022-12-22 12:17:31 +0100

    add nameconverter.cpp

---
+++ include/nameconverter.hpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2022  Timo Früh
+// Full copyright notice in src/main.cpp
+
+
+#include <vector>
+#include <string>
+
+namespace rnc {}
---
+++ src/nameconverter.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2022  Timo Früh
+// Full copyright notice in main.cpp
+
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include "nameconverter.hpp"
+
