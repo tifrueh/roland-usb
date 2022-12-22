0197f3362d2d458627d220639a2fe1b7a50a8834
Timo Früh <timo.frueh@icloud.com>
2022-12-22 12:34:26 +0100

    add documentation

--- include/nameconverter.hpp
+++ include/nameconverter.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 #include <string>
 
+// namespace: roland nameconverter
 namespace rnc {
+
+    // returns a vector containing all the lines of a titles.csv file
     std::vector<std::string> parseTitlesCSV(const std::string& titlesCsvPath);
 }
