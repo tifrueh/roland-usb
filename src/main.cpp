cb215cea51e71413a40cf2fa6b9cb67e7e4b95a7
Timo Früh <timo.frueh@icloud.com>
2022-12-23 16:35:38 +0100

    implement CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,41 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+#define VERSION "1.0.0-dev"
+
 #include <filesystem>
+#include <string>
 #include "CLI11.hpp"
 #include "subcommands.hpp"
 
 int main(int argc, char** argv) {
+
+    CLI::App app = CLI::App("Manage the renaming of audio files for the Roland FP-30 Digital Piano", "roland-usb");
+
+    app.set_version_flag("-v,--version", VERSION, "Print version and exit");
+
+    std::string sTargetDirectory;
+    app.add_option("-d,--directory", sTargetDirectory, "Target directory");
+    std::filesystem::path targetDirectory = sTargetDirectory;
+
+    CLI::App* scmdInit = app.add_subcommand("init", "Initialise target directory");
+    CLI::App* scmdEncrypt = app.add_subcommand("encrypt", "Rename .wav files in target directory to FP-30 readable names");
+    CLI::App* scmdEncrypt = app.add_subcommand("decrypt", "Rename .wav files in target directory to human readable names");
+    app.require_subcommand(1, 1);
+
+    if (*scmdInit) {
+        scmd::init(targetDirectory);
+    }
+    else if (*scmdEncrypt) {
+        scmd::encrypt(targetDirectory);
+    }
+    else if (*scmdInit) {
+        scmd::decrypt(targetDirectory);
+    }
+    else {
+        throw std::logic_error("no valid subcommand given");
+    }
+
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
