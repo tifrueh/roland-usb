20f86ebed8f528b022098727e80f88b8c7e801c9
Timo Früh <timo.frueh@icloud.com>
2022-12-23 17:19:48 +0100

    set version string

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-#define VERSION "1.0.0-alpha"
+#define VERSION "1.0.0-dev"
 
 #include <filesystem>
 #include <string>
